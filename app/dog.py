f3e6498bc068aa7b57c2f9cd7bc647f5ecdde746
Madeline Stark <17558341+Madeline-Stark@users.noreply.github.com>
2023-01-08 10:18:51 -0800

    pass

--- app/dog.py
+++ app/dog.py
@@ -1,27 +1,51 @@
 from sqlalchemy import create_engine
 
+# student needs to add
+from models import Dog
+
 engine = create_engine('sqlite:///:memory:')
 
 def create_table(base):
-    pass
+    # contains function "create_table()" that 
+    # takes a declarative_base,
+    # test invokes like this: engine = create_table(Base)
+
+    # creates table "dogs" if it does not exist,
+    # https://docs.sqlalchemy.org/en/14/orm/quickstart.html#emit-create-table-ddl
+    #  https://docs.sqlalchemy.org/en/14/core/metadata.html#sqlalchemy.schema.MetaData.create_all
+    # from documentation: 
+    # method sqlalchemy.schema.MetaData.create_all(bind=None, tables=None, checkfirst=True) 
+    # from solution: 
+    base.metadata.create_all(engine)
+    # and returns the engine.
+    return engine
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session
 
-def new_from_db(session):
-    pass
+def new_from_db(session, row):
+    # contains function "new_from_db()" that takes a database row 
+    # need to pass in the row
+    # and returns a Dog instance.
+    # so just selecting that dog from the db
+    return session.query(Dog).filter_by(id = row.id).first()
 
 def get_all(session):
-    pass
+    return [dog for dog in session.query(Dog)]
 
 def find_by_name(session, name):
-    pass
+    # how does name = name work? does it know the difference?
+    return session.query(Dog).filter_by(name = name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id = id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name = name, breed = breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed 
+    session.commit()
+    return session
--- app/models.py
+++ app/models.py
@@ -6,4 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Dog(Base):
-    pass
+    # Remember which attributes are required when designing a SQLAlchemy data model: 
+    # a __tablename__, 
+    __tablename__ = 'dogs'
+    # a primary_key, 
+    # from the documentation and defining a schema lesson:
+    id = Column(Integer, primary_key=True)
+    # from the solution:
+    #  __table_args__ = (PrimaryKeyConstraint('id'),)
+
+    # id = Column(Integer())
+    
+    # and one or more Columns
+    name = Column(String())
+    breed = Column(String())
